3c83049e86c05c8b0828dbe1b1e1070a0c8ebccf
Thad Boyd <thad@daddyslick.com>
2015-09-18 13:34:00 -0700

    Early configuration

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php get_header(); ?>
+      
+      <div id="content">      
+        <div id="inner-content" class="row">
+        
+          <?php get_sidebar(); ?>
+    
+          <main id="main" class="columns medium-8 medium-pull-4 large-9 large-pull-3" role="main">
+          <!-- To see additional archive styles, visit the /parts directory -->
+            <?php get_template_part( 'parts/loop', 'archive' ); ?>    
+          </main> <!-- end #main -->            
+        </div> <!-- end #inner-content -->
+      </div> <!-- end #content -->
+
+<?php get_footer(); ?>
---
+++ sidebar.php
@@ -0,0 +1,14 @@
+<div id="sidebar1" class="hide-for-small sidebar columns medium-4 medium-push-8 large-3 large-push-9" role="complementary">
+  <?php if ( is_active_sidebar( 'sidebar1' ) ) : ?>
+    <?php dynamic_sidebar( 'sidebar1' ); ?>
+  <?php else : ?>
+
+  <!-- This content shows up if there are no widgets defined in the backend. -->
+            
+  <div class="alert help">
+    <p><?php _e("Please activate some Widgets.", "jointstheme");  ?></p>
+  </div>
+
+  <?php endif; ?>
+
+</div>

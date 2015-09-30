09a0c5179ca15dca9c1476dd3972a497174370d6
Thad Boyd <thad@daddyslick.com>
2015-09-30 12:48:05 -0700

    Fixed styling of pagination bar; added pagination bar to top of page

--- parts/loop-archive.php
+++ parts/loop-archive.php
@@ -1,3 +1,5 @@
+<?php joints_page_navi(); ?>
+
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 	
 	<article id="post-<?php the_ID(); ?>" <?php post_class(''); ?> role="article">					
